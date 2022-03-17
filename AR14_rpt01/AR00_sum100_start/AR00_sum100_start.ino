50141703267300c681690bd606bc9c723639eb59
wogk0012 <100752553+wogk0012@users.noreply.github.com>
2022-03-17 17:02:59 +0900

    Add files via upload

---
+++ AR14_rpt01/AR00_sum100_start/AR00_sum100_start.ino
@@ -0,0 +1,33 @@
+/*
+ DIY-3
+*/
+
+// start number
+int number = 0;
+int sum = 0;
+
+void setup() {
+  // 9600bps로 시리얼 통신 설정
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  number++;
+  sum += number;
+  Serial.print("Number = ");
+  Serial.print(number);
+  Serial.print(" , Sum = ");
+  Serial.println(sum);  
+  
+  if(number == 100) {
+    Serial.print("ARnn : 1+2+...+100+5050");
+delay(1000);
+    exit(0);
+
+
+    
+  }
+  
+  delay(100);  // 0.1초동안 지연시킨다.
+}
